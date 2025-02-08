b8fec5712b598d0e7830003e6e401d624385ea5a
Siswadi24 <danaperdanaputra32@gmail.com>
2025-02-08 11:11:59 +0700

    style: changes style dark mode at HeaderWhite section

--- components/layouts/HeaderWhite.vue
+++ components/layouts/HeaderWhite.vue
@@ -7,7 +7,7 @@
                     <BaseLogo color="orange" />
                     <template v-if="route.meta?.header?.title">
                         <hr class="w-[1px] h-5 bg-primary">
-                        <p class="text-xs md:text-xl">{{ route.meta?.header?.title }}</p>
+                        <p class="text-xs md:text-xl dark:text-black">{{ route.meta?.header?.title }}</p>
                     </template>
                 </div>
                 <LayoutsSearchBar v-if="route.meta?.header?.showSearch" class="w-1/2" :padded="false" />
@@ -19,6 +19,7 @@
 <script setup>
 const route = useRoute()
 
+// -- Note Example Meta --
 // const meta = {
 //     showProfile: false,
 //     title: 'Keranjang',
@@ -27,6 +28,10 @@ const route = useRoute()
 </script>
 
 <style scoped>
+.header-bottom {
+    @apply dark:bg-white;
+}
+
 .header-bottom-container {
     @apply flex justify-between items-center gap-5 md:gap-10;
     @apply py-3 md:py-7;

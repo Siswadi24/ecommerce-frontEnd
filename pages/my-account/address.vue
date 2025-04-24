4322385e27ed4b114e07e0ebf7f2c8e498451837
Siswadi24 <danaperdanaputra32@gmail.com>
2025-04-24 22:16:44 +0700

    feat:Implementation slicing UI Profile Akun Address Section

--- pages/my-account/address.vue
+++ pages/my-account/address.vue
@@ -1,17 +1,67 @@
 <template>
-    <div>
-       <div class="flex justify-between gap-4 items-center">
-        <h3 class="text-lg font-medium">Alamat Saya</h3>
-        <UButton icon="i-heroicons:plus">Tambah Alammat Saya</UButton>
-       </div>
-       <hr>
+  <div>
+    <div class="flex justify-between gap-4 items-center">
+      <h3 class="text-lg font-medium">Alamat Saya</h3>
+      <UButton icon="i-heroicons:plus">Tambah Alammat Saya</UButton>
     </div>
+    <hr class="mt-5 mb-3 border-gray-200/60" />
+    <h4 class="text-lg font-normal">Alamat</h4>
+    <div class="divide-y">
+      <FeatureProfileAddressCardItem
+        v-for="address in addressList"
+        :key="address.uuid"
+        :item="address"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { add } from "date-fns";
 
+const addressList = [
+  {
+    uuid: "8c438746-79c5-11ef-b707-97a05ceb87e2",
+    is_default: true,
+    receiver_name: "Azis Hapidin",
+    receiver_phone: "08888",
+    city: {
+      uuid: "ee8eb26c-78fe-11ef-bd77-9e4478916c69",
+      province: {
+        uuid: "ee8d857c-78fe-11ef-bd77-9e4478916c69",
+        name: "Bali",
+      },
+      external_id: 128,
+      name: "Kabupaten Gianyar",
+    },
+    district: "Bojong",
+    postal_code: "43222",
+    detail_address: "Jl. ABC No. 123",
+    address_note: "Dekat tugu pahlawan",
+    type: "home",
+  },
+  {
+    uuid: "953bc2be-79c5-11ef-b707-97a05ceb87e2",
+    is_default: false,
+    receiver_name: "Azis Hapidin",
+    receiver_phone: "08888",
+    city: {
+      uuid: "ee8eb26c-78fe-11ef-bd77-9e4478916c69",
+      province: {
+        uuid: "ee8d857c-78fe-11ef-bd77-9e4478916c69",
+        name: "Bali",
+      },
+      external_id: 128,
+      name: "Kabupaten Gianyar",
+    },
+    district: "Bojong",
+    postal_code: "43222",
+    detail_address: "Jl. ABC No. 123",
+    address_note: "Dekat tugu pahlawan",
+    type: "home",
+  },
+];
 </script>
 
 <style lang="scss" scoped>
-
 </style>

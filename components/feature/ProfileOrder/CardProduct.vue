b57c840fc3e95c226d1bbebadc62ce566420ca0f
Siswadi24 <danaperdanaputra32@gmail.com>
2025-05-09 20:56:36 +0700

    feat:Slicing UI Pop Up review product Section

--- components/feature/ProfileOrder/CardProduct.vue
+++ components/feature/ProfileOrder/CardProduct.vue
@@ -2,48 +2,49 @@
   <div class="flex gap-3">
     <img
       src="https://picsum.photos/1920/1080?random=1"
-      class="aspect-[1/1] h-40 w-40 object-cover"
+      class="aspect-[1/1] object-cover"
+      :class="sizeClasse[size]?.image"
     />
     <div class="flex-1">
-      <p class="line-clamp-2">Product name</p>
-      <p class="text-sm text-black/55">Variasi: White, 39</p>
-      <p class="text-sm">x1</p>
-      <template>
+      <p class="line-clamp-2" :class="sizeClasse[size]?.title">Product name</p>
+      <template v-if="!hideDescription">
         <p class="text-sm text-black/55">Variasi: White, 39</p>
         <p class="text-sm">x1</p>
       </template>
     </div>
-    <div class="flex items-center">Rp{{ formatNumber(1000) }}</div>
+    <div v-if="!hidePrice" class="flex items-center">
+      Rp{{ formatNumber(1000) }}
+    </div>
   </div>
 </template>
   
 <script setup>
-// defineProps({
-//   hidePrice: {
-//     type: Boolean,
-//     default: false,
-//   },
-//   hideDescription: {
-//     type: Boolean,
-//     default: false,
-//   },
-//   size: {
-//     type: String,
-//     default: "lg",
-//     validator: (propsValue) => ["sm", "lg"].includes(propsValue),
-//   },
-// });
+defineProps({
+  hidePrice: {
+    type: Boolean,
+    default: false,
+  },
+  hideDescription: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: String,
+    default: "lg",
+    validator: (propsValue) => ["sm", "lg"].includes(propsValue),
+  },
+});
 
-// const sizeClasse = {
-//   sm: {
-//     image: "h-14 w-14",
-//     title: "text-sm",
-//   },
-//   lg: {
-//     image: "h-20 w-20",
-//     title: "text-base",
-//   },
-// };
+const sizeClasse = {
+  sm: {
+    image: "h-14 w-14",
+    title: "text-sm",
+  },
+  lg: {
+    image: "h-20 w-20",
+    title: "text-base",
+  },
+};
 </script>
   
 <style lang="scss" scoped></style>
--- pages/my-account/orders/index.vue
+++ pages/my-account/orders/index.vue
@@ -89,6 +89,94 @@
         </div>
       </template>
     </UCard>
+
+    <UModal v-model="openReview">
+      <UCard class="text-black/85 text-sm">
+        <p class="text-xl font-normal">Nilai Produk</p>
+        <div class="space-y-5 mt-8">
+          <div
+            class="border border-yellow-500 bg-yellow-50 rounded-sm py-2 px-3 flex gap-2"
+          >
+            <IconCoinSolid />
+            <span class="font-medium">Beri penilaian & dapatkan 25 Koin!</span>
+          </div>
+          <div class="space-y-2">
+            <FeatureProfileOrderCardProduct
+              v-for="a in 3"
+              :key="`product-${a}`"
+              hide-price
+              hide-description
+              size="sm"
+            />
+          </div>
+          <div class="flex gap-2 items-center">
+            <span class="w-44">Kualitas Produk</span>
+            <BaseRating v-model="formRating.rating" size="lg" color="yellow" />
+            <span
+              :class="{
+                'text-yellow-500': formRating.rating > 3,
+                'text-black/55': formRating.rating <= 3,
+              }"
+              >{{ reviewMessages[formRating.rating - 1] }}</span
+            >
+          </div>
+          <div>
+            <div class="bg-gray-100 px-6 py-3 rounded-sm space-y-3">
+              <UTextarea
+                placeholder="Bagikan penilaianmu tentang produk ini untuk membantu Pembeli lain."
+              />
+              <div class="flex gap-2">
+                <UButton
+                  size="xs"
+                  variant="soft"
+                  icon="i-heroicons:camera-solid"
+                  label="Tambah Foto"
+                />
+                <UButton
+                  size="xs"
+                  variant="soft"
+                  icon="i-heroicons:video-camera-solid"
+                  label="Tambahkan video"
+                />
+              </div>
+              <p class="text-right text-black/55">
+                Tambahkan 50 karakter dengan 1 foto untuk mendapatkan 25 Koin!
+              </p>
+            </div>
+          </div>
+          <div class="space-y-4">
+            <p class="text-base">Tentang Layanan</p>
+            <div class="flex gap-2 items-center">
+              <span class="w-44">Kecepatan Jasa Kirim</span>
+              <BaseRating
+                v-model="formRating.courierRating"
+                size="lg"
+                color="yellow"
+              />
+              <span
+                :class="{
+                  'text-yellow-500': formRating.courierRating > 3,
+                  'text-black/55': formRating.courierRating <= 3,
+                }"
+                >{{ reviewMessages[formRating.courierRating - 1] }}</span
+              >
+            </div>
+          </div>
+          <div class="flex justify-end gap-2 pt-6">
+            <UButton
+              class="min-w-36 justify-center"
+              variant="link"
+              color="gray"
+              @click="openReview = false"
+              >Nanti Saja</UButton
+            >
+            <UButton class="min-w-36 justify-center" @click="openReview = false"
+              >Ok</UButton
+            >
+          </div>
+        </div>
+      </UCard>
+    </UModal>
   </div>
 </template>
 
@@ -96,6 +184,21 @@
 definePageMeta({
   wrapper: "div",
 });
+
+const openReview = ref(false);
+
+const formRating = reactive({
+  rating: 0,
+  courierRating: 0,
+});
+
+const reviewMessages = [
+  "Sangat Buruk",
+  "Buruk",
+  "Biasa",
+  "Baik",
+  "Sangat Baik",
+];
 
 const items = [
   {
